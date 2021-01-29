f1dcc12ad10f71ddb378c5544e1d9f41e9d8088d
mathew461 <mathew461@gmail.com>
2021-01-29 23:04:58 +0000

    [Daily Commit 29/01/2021]

---
+++ jsx/src/index.js
@@ -0,0 +1,33 @@
+//Import the React and ReactDOM libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// Create a react component
+const App = () => {
+
+    const buttonText = 'Click Me!'
+
+    return (
+        <div> 
+            <label class='label' htmlFor="name">Enter Name</label>
+            <input id="name" type="text" />
+            <button style={{backgroundColor: 'blue', color: 'white'}}>
+                {buttonText}
+            </button>
+        </div>
+    );
+};
+
+if (module.hot) {
+    module.hot.accept();
+  }    
+
+
+  
+// Take the react component and show it on the screen
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+);
+
+
