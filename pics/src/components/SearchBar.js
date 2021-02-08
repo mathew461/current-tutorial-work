f121780980c1559db1cc29ef02d6f9cab6d2915d
mathew461 <mathew461@gmail.com>
2021-02-08 19:11:03 +0000

    [Daily Commit 08/02/2021 - completed quite a large chunk of the tutorial today, already halfway through section 9. Building Lists of Records]

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -5,26 +5,26 @@ class SearchBar extends React.Component {
 
     onFormSubmit = event => {
         event.preventDefault();
-
+    
         this.props.onSubmit(this.state.term);
     };
 
     render() {
         return (
-            <div className="ui segment">
-                <form onSubmit={this.onFormSubmit} className="ui form">
-                    <div className="field">
-                        <label>Image Search</label>
-                        <input 
-                            type="text" 
-                            value={this.state.term} 
-                            onChange={(e) => this.setState({ term: e.target.value })} 
-                        />
-                    </div>
-                </form>
-            </div>
-        )
+          <div className="ui segment">
+            <form onSubmit={this.onFormSubmit} className="ui form">
+              <div className="field">
+                <label>Image Search</label>
+                <input
+                  type="text"
+                  value={this.state.term}
+                  onChange={e => this.setState({ term: e.target.value })}
+                />
+              </div>
+            </form>
+          </div>
+        );
+      }
     }
-}
 
 export default SearchBar;
