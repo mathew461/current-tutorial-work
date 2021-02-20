63ef07f83a935ad14c531a9c73bb6e93be4473d4
mathew461 <mathew461@gmail.com>
2021-02-20 19:31:47 +0000

    [Daily Commit 20/02/2021 - Finished Videos Project]

--- videos/src/apis/youtube.js
+++ videos/src/apis/youtube.js
@@ -1,1 +1,15 @@
+import axios from 'axios';
+
 const KEY = 'AIzaSyBTm9HO_HP9DVeNRqg0SH380jbhH4zN_iI';
+
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params: {
+        part: 'snippet',
+        type: 'video',
+        maxResults: 5,
+        key: KEY
+    }
+});
+
