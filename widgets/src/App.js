9e2dcab96c0927494fab5430d78de62ec005d094
mathew461 <mathew461@gmail.com>
2021-03-04 17:54:23 +0000

    [Daily Commit 04/03/2021 - worked on Widgets project ]

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Accordion from './Components/Accordion';
+import Search from './Components/Search';
 
 const items = [
     {
@@ -19,7 +20,7 @@ const items = [
 export default () => {
     return (
         <div> 
-            <Accordion items={items} />
+            <Search />
         </div>
     )
 };
