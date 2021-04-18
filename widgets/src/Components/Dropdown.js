7579ec31740f2e8416c69c589072b2ca94354ea2
mathew461 <mathew461@gmail.com>
2021-04-18 22:57:41 +0100

    [Upto 49. Important Update for React v17]

--- widgets/src/Components/Dropdown.js
+++ widgets/src/Components/Dropdown.js
@@ -1,7 +1,14 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
     const [open, setOpen] = useState(false);
+
+    useEffect(() => {
+        document.body.addEventListener('click', (event) => {
+            console.log(event.target) // shows in console log what section was clicked on!! Use in future to learn more about site structure
+            setOpen(false)
+        }, { capture: true});
+    }, []);
 
     const renderedOptions = options.map((option) => {
         if (option.value === selected.value) {
