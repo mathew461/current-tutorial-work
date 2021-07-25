a8660dedb88a57122f04f880e8bac29df6c9bad2
mathew461 <mathew461@gmail.com>
2021-07-25 14:00:17 +0100

    [Routine Backup]

--- streams/client/src/actions/index.js
+++ streams/client/src/actions/index.js
@@ -1,3 +1,4 @@
+import streams from '../apis/streams';
 import { SIGN_IN, SIGN_OUT } from './types';
 
 export const signIn = (userId) => {
@@ -13,4 +14,6 @@ export const signOut = () => {
     };
 };
 
- 
+export const createStream = (formValues) => async (dispatch) => {
+    streams.post('/streams', formValues);
+};
